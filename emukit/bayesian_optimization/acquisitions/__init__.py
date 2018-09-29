a9fca68bf99d3a71f167ed3f9e9fca44940155b8
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2018-09-29 12:40:54 +0200

    add logei

--- emukit/bayesian_optimization/acquisitions/__init__.py
+++ emukit/bayesian_optimization/acquisitions/__init__.py
@@ -2,3 +2,4 @@ from .expected_improvement import ExpectedImprovement
 from .negative_lower_confidence_bound import NegativeLowerConfidenceBound
 from .probability_of_improvement import ProbabilityOfImprovement
 from .entropy_search import EntropySearch
+from .log_expected_improvement import LogExpectedImprovement
