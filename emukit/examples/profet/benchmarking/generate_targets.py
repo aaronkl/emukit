e0d1dcfc072fb3f44d9e632621ed0711a2a0d3a9
Aaron Klein <kleinaa@informatik.uni-freiburg.de>
2019-06-04 18:26:44 +0200

    add fcnet

--- emukit/examples/profet/benchmarking/generate_targets.py
+++ emukit/examples/profet/benchmarking/generate_targets.py
@@ -1,19 +1,13 @@
-import os
 import argparse
 import json
+import os
+
 import numpy as np
 import sobol_seq
 
-from emukit.examples.profet.meta_benchmarks import meta_svm, meta_forrester
-
-from emukit.examples.gp_bayesian_optimization.enums import ModelType, AcquisitionType
-from emukit.examples.gp_bayesian_optimization.optimization_loops import create_bayesian_optimization_loop
-from emukit.examples.gp_bayesian_optimization.single_objective_bayesian_optimization import GPBayesianOptimization
-from emukit.benchmarking.loop_benchmarking.benchmarker import Benchmarker
-from emukit.benchmarking.loop_benchmarking.metrics import MinimumObservedValueMetric, TimeMetric, CumulativeCostMetric
+from emukit.examples.profet.meta_benchmarks import meta_svm, meta_forrester, meta_fcnet
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--run_id', default=0, type=int, nargs='?')
 parser.add_argument('--output_path', default="./", type=str, nargs='?')
 parser.add_argument('--sample_path', default="./", type=str, nargs='?')
 parser.add_argument('--benchmark', default="meta_svm", type=str, nargs='?')
@@ -23,7 +17,6 @@ args = parser.parse_args()
 targets = []
 
 for instance_id in range(1000):
-
     if args.benchmark == "meta_forrester":
 
         fname_objective = os.path.join(args.sample_path, "sample_objective_%d.pkl" % instance_id)
@@ -34,8 +27,15 @@ for instance_id in range(1000):
         fname_objective = os.path.join(args.sample_path, "sample_objective_%d.pkl" % instance_id)
         fname_cost = os.path.join(args.sample_path, "sample_cost_%d.pkl" % instance_id)
         fcn, parameter_space = meta_svm(fname_objective=fname_objective,
-                                    fname_cost=fname_cost,
-                                    noise=False)
+                                        fname_cost=fname_cost,
+                                        noise=False)
+    elif args.benchmark == "meta_fcnet":
+
+        fname_objective = os.path.join(args.sample_path, "sample_objective_%d.pkl" % instance_id)
+        fname_cost = os.path.join(args.sample_path, "sample_cost_%d.pkl" % instance_id)
+        fcn, parameter_space = meta_fcnet(fname_objective=fname_objective,
+                                          fname_cost=fname_cost,
+                                          noise=False)
 
     D = len(parameter_space.parameters)
     X = sobol_seq.i4_sobol_generate(D, D * 100)
@@ -50,7 +50,6 @@ for instance_id in range(1000):
             y.append(float(fcn(xi[None, :])[0, 0]))
         else:
             y.append(float(fcn(xi[None, :])[0][0, 0]))
-
     targets.append(y)
 
 print(np.array(targets).shape)
